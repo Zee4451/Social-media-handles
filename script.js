e7bc2d9a77754466d8545e2103a31635ebb0af6d
Zee4451 <159883334+Zee4451@users.noreply.github.com>
2024-03-28 00:23:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function openfb(){
+    const url1 = "https://www.facebook.com/rex.khan3"
+    window.open(url1,)
+}
+function openIn(){
+    const url1 = "https://www.instagram.com/musharrifkhan/"
+    window.open(url1,)
+}
+function openWa(){
+    const url1 = "https://wa.link/ntso2j"
+    window.open(url1,)
+}
+function openTg(){
+    const url1 = "https://t.me/SfactorDanceCrew"
+    window.open(url1,)
+}
+function openCt(){
+    const url1 = "tel:9340073167"
+    window.open(url1,)
+}
+function openGm(){
+    const url1 = "mailto:sfactorperformingart@gmail.com"
+    window.open(url1,)
+}
